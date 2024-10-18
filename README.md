3dc196301a12a397443165a23adc4778cd1e1d63
Bennett Smrdel <fireknights318@gmail.com>
2024-10-17 19:27:41 -0500

    feat: Can compile program

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# C-test
+# Installation
+
+1. Download UCRT (with MCF threads) [https://winlibs.com](https://winlibs.com)
+2. Run the program `./dev.sh`
--- main.c
+++ main.c
@@ -1,24 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-struct Item {
-  char name[10];
-  int qty;
-};
+#define clrscr() system("@cls||clear")
 
-struct Inventory {
+typedef struct Item {
+  char name[20];
+  int qty;
+} Item;
+
+typedef struct Inventory {
   struct Item *items;
-};
+} Inventory;
 
 int main() {
-  struct Inventory inventory;
-  inventory.items = (struct Item*) malloc(100 * sizeof(struct Item));
-  struct Item sword = {"Sword", 1};
+  Inventory inventory;
+  inventory.items = (Item*) malloc(100 * sizeof(Item));
+  Item sword = {"Sword", 1};
   inventory.items[0] = sword;
   
+  clrscr();
   printf("INVENTORY:\n");
   for (int i = 0; i < 1; i++) {
-    printf("* %d %s\n", inventory.items[i].qty, inventory.items[i].name);
+    printf("- %d %s\n", inventory.items[i].qty, inventory.items[i].name);
   }
   
   free(inventory.items);
